44b5e3a9463be8a9c3d85802c1f6e4dd33cd6422
Yae Jin Park <yaejin91@gmail.com>
2015-09-21 11:46:20 -0700

    test-app updated

--- test-app/js/battleship-test.js
+++ test-app/js/battleship-test.js
@@ -4,8 +4,14 @@
 //Test shipAnchor (element of shipSpot)
 //Test passwordCheck (array)
 
-assert(newGame == 1, "New game starts.");
-assert(userInfo != [], "User typed in username and password");
-assert(shipSpot >= 0, "Ship coordinates are valid");
-assert(shipAnchor == shipSpot[0], "Ship's anchor point works");
-assert(passwordCheck == userInfo[1], "Password is valid/invalid");
+var testnewGame = function(){
+	//create test here that clicks the new Game button
+
+	return false;
+}
+
+assert(testnewGame, "New game starts.");
+// assert(userInfo != [], "User typed in username and password");
+// assert(shipSpot >= 0, "Ship coordinates are valid");
+// assert(shipAnchor == shipSpot[0], "Ship's anchor point works");
+// assert(passwordCheck == userInfo[1], "Password is valid/invalid");
